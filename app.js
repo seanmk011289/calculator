8782284502d48a896243ee09eb5214a07349587b
Sean Kennedy <seanmk011289@gmail.com>
2024-06-18 20:38:45 -0500

    Added functionality to account for post equation

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ let nums = [...document.querySelectorAll(".num")];
 let ops = [...document.querySelectorAll(".op")];
 let equal = document.querySelector(".eq");
 let active = document.querySelector("[data-active]");
-let inactive = document.querySelector("[data-inactive]");
+let clear = document.querySelector(".clear");
 
-let first = "";
-let second = "";
-let func = "";
-// let firstReset = true;
-// let secondReset = false;
-let stat = "first";
-let answer;
+// let inactive = document.querySelector("[data-inactive]");
 
+let first, second, func, stat, answer;
+
+let initiatlize = function (foo, one) {
+  first = one;
+  second = "";
+  func = "";
+  // let firstReset = true;
+  // let secondReset = false;
+  stat = foo;
+  answer;
+  return;
+};
+
+// OPERATIONS
 let add = function (a, b) {
   return parseFloat(a) + parseFloat(b);
 };
@@ -28,67 +36,80 @@ let divide = function (a, b) {
   return parseFloat(a) / parseFloat(b);
 };
 
+// OPERATE FUNCTION
 let operate = function (foo, a, b) {
   switch (foo) {
     case "+":
-      answer = add(a, b);
+      answer = Math.round(add(a, b) * 100) / 100;
       break;
     case "-":
-      answer = subtract(a, b);
+      answer = Math.round(subtract(a, b) * 100) / 100;
       break;
     case "*":
-      answer = multiply(a, b);
+      answer = Math.round(multiply(a, b) * 100) / 100;
       break;
     case "/":
-      answer = divide(a, b);
+      answer = Math.round(divide(a, b) * 100) / 100;
       break;
   }
   return answer;
 };
 
+// Update the HTML with the Answer
 let updateActive = function (num) {
   active.textContent = num;
 };
 
 nums.forEach((num) => {
   num.addEventListener("click", (e) => {
+    // Checking for solved status, if they've already solved one problem
+    if (stat == "solved") {
+      initiatlize("first", "");
+      updateActive(first);
+    }
     if (stat == "first") {
       first += e.target.dataset.num;
       updateActive(first);
-    }
-    if (stat == "second") {
+      console.log(first, second);
+    } else if (stat == "second") {
       second += e.target.dataset.num;
       updateActive(second);
+      console.log(first, second);
     }
   });
 });
 
 ops.forEach((op) => {
   op.addEventListener("click", (e) => {
+    if (second != "") {
+      operate(func, first, second);
+      updateActive(answer);
+      initiatlize("solved", answer);
+    }
     func = e.target.textContent;
     stat = "second";
-    active.textContent = "0";
-    firstPart = `${first} ${func}`;
-    inactive.textContent = firstPart;
   });
 });
 
 equal.addEventListener("click", (e) => {
-  // let answer;
-  // switch (func) {
-  //   case "add":
-  //     answer = add(first, second);
-  //     break;
-  //   case "subtract":
-  //     answer = subtract(first, second);
-  //     break;
-  //   case "multiply":
-  //     answer = multiply(first, second);
-  //     break;
-  //   case "divide":
-  //     answer = divide(first, second);
-  //     break;
-  // }
-  operate(func, first, second);
-  console.log(answer);
+  // Dividing by 0
+  if (func == "/" && second == 0) {
+    updateActive("Don't even think about it dude");
+    initiatlize("first", "");
+    return;
+  }
+  if (first != "" && second != "") {
+    operate(func, first, second);
+    updateActive(answer);
+    initiatlize("solved", answer);
+  } else {
+    return;
+  }
 });
+
+clear.addEventListener("click", (e) => {
+  initiatlize("first", "");
+  updateActive(0);
+});
+
+initiatlize("first", "");
